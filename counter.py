ccba4c6658eeee9dd2805edeb1892b7eebd8920e
Kimber <kimberly.winter@students.olin.edu>
2016-10-13 16:03:39 -0400

    Add existing file.

--- counter.py
+++ counter.py
@@ -3,37 +3,21 @@
 from os.path import exists
 import sys
 from pickle import dump, load
+import pickle #I'm just importing all of pickle lol
+counter=0 #initializing the counter
 
-def update_counter(file_name, reset=False):
-	""" Updates a counter stored in the file 'file_name'
+def update_counter(file_name, reset):
+	if(reset==True): #if reset is true, reset to counter 1
+		counter=1
+		pickle.dump(counter, open(file_name,'wb'))
+	if(reset==False): #if reset is not true, either create a new file or adds 1 to the counter
+		try:
+			counter= int(pickle.load(open(file_name, 'rb')))+1
+			pickle.dump(counter, open(file_name, 'wb'))
+		except:
+			pickle.dump(1,open(file_name,'wb'))
 
-		A new counter will be created and initialized to 1 if none exists or if
-		the reset flag is True.
 
-		If the counter already exists and reset is False, the counter's value will
-		be incremented.
-
-		file_name: the file that stores the counter to be incremented.  If the file
-				   doesn't exist, a counter is created and initialized to 1.
-		reset: True if the counter in the file should be rest.
-		returns: the new counter value
-
-	>>> update_counter('blah.txt',True)
-	1
-	>>> update_counter('blah.txt')
-	2
-	>>> update_counter('blah2.txt',True)
-	1
-	>>> update_counter('blah.txt')
-	3
-	>>> update_counter('blah2.txt')
-	2
-	"""
-	pass
-
-if __name__ == '__main__':
-	if len(sys.argv) < 2:
-		import doctest
-		doctest.testmod()
-	else:
-		print "new value is " + str(update_counter(sys.argv[1]))
+#update_counter('test3.txt',False)						#these sections are for debugging! :)
+#counter= pickle.load(open('test3.txt', 'rb'))
+#print counter
