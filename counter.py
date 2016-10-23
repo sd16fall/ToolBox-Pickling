718066af842e56ffa4c7a4ad1ae6166431825993
Brennan VandenHoek <Brennan@Brennans-MacBook-Pro-2.local>
2016-10-23 14:36:41 -0400

    Turning in Pickling Toolbox

--- counter.py
+++ counter.py
@@ -2,7 +2,8 @@
 
 from os.path import exists
 import sys
-from pickle import dump, load
+import pickle
+import anydbm
 
 def update_counter(file_name, reset=False):
 	""" Updates a counter stored in the file 'file_name'
@@ -29,7 +30,22 @@ def update_counter(file_name, reset=False):
 	>>> update_counter('blah2.txt')
 	2
 	"""
-	pass
+
+	db = anydbm.open('update_counter.db', 'c') #created new database if there wasn't already one
+	file_name_str = str(file_name) #turns filename into string just incase it is not already
+
+	if reset == True: #creates new line if input is true
+		count = 1
+		db[file_name_str] = pickle.dumps(count)
+	else: #reads from old input and incriments it one
+		count = pickle.loads(db[file_name_str]) + 1
+		db[file_name_str] = pickle.dumps(count)
+
+	print count #prints the current count of the db line
+
+	db.close() #closes database
+
+############################### DOC TEST #######################################
 
 if __name__ == '__main__':
 	if len(sys.argv) < 2:
